94107d70ab7bf33104af6d591c528b48a60ce696
fxh <1745955087@qq.com>
2022-02-10 21:07:36 +0800

    细节

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="topbar bg-black py-2 px-3 d-flex ai-center">
-            <img src="../assets/logo.png" height="30">
+            <a href="/" class="sprite sprite-logo"></a>
             <div class="px-2 flex-1">
                 <div class="text-white fs-md">王者荣耀</div>
                 <div class="text-grey-1 fs-sm">团队成就更多</div>
